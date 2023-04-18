d5d0c1b7df26021aaaa9b48de2d54b495ffcbfd2
benthecoder <benthecoder07@gmail.com>
2023-04-18 01:00:56 -0500

    Create getPostContent function

---
+++ components/getPostContent.ts
@@ -0,0 +1,12 @@
+import matter from 'gray-matter';
+import fs from 'fs';
+
+const getPostContent = (slug: string) => {
+  const folder = 'posts/';
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, 'utf8');
+  const matterResult = matter(content);
+  return matterResult;
+};
+
+export default getPostContent;
