229c7b0095eeee3f244dbb950e2176c42c3a0786
benthecoder <benthecoder07@gmail.com>
2023-04-18 16:12:56 -0500

    Add Analytics and cleaned up CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,37 @@
 import Link from 'next/link';
 import './globals.css';
 
+import { AnalyticsWrapper } from '../components/analytics';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const Sidebar = (
-    <div className='flex flex-col items-center'>
+    <div className='flex flex-col items-center md:items-end md:w-52'>
       <div>
         <Link href='/'>
-          <h2 className=' text-black text-center'>BENEDICT NEO</h2>
+          <h2 className='text-black font-semibold'>BENEDICT NEO</h2>
         </Link>
       </div>
-      <div className='text-slate-500 flex flex-row space-x-3 md:flex-col md:justify-center md:w-full md:items-end md:mt-10'>
-        <Link href='/links'>links</Link>
+      <div className='text-slate-500 flex flex-row space-x-3 md:flex-col md:mt-10 md:items-end'>
+        <Link href='/about'>whoami</Link>
         <Link href='/tags'>tags</Link>
+        <Link href='/links'>links</Link>
         <Link href='/posts'>archive</Link>
-        <Link href='/about'>about</Link>
       </div>
     </div>
   );
 
   return (
     <html lang='en'>
-      <body className='max-w-3xl font-cormorant mb-40 flex flex-col mx-auto md:flex-row mt-4 md:mt-20 lg:mt-20'>
+      <body className='max-w-3xl font-cormorant flex flex-col mb-40 mx-auto mt-4 md:flex-row md:mt-20'>
         {Sidebar}
-        <main className='flex-auto text-sm min-w-0 mx-3 mt-5 md:mt-0 flex flex-col px-10'>
+        <main className='flex-auto text-sm min-w-0 max-w-lg mt-10 md:mt-0 px-16'>
           {children}
         </main>
+        <AnalyticsWrapper />
       </body>
     </html>
   );
