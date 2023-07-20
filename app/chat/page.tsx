721b59d4c8eebe591113de32475ade1811cdd606
benthecoder <benthecoder07@gmail.com>
2023-07-19 21:35:12 -0700

    Change chat api and design
    Using OpenAIStream

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -6,21 +6,24 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className='mx-auto w-full max-w-md py-24 flex flex-col stretch'>
+    <div className=' w-full max-w-md flex flex-col stretch'>
       {messages.length > 0
         ? messages.map((m) => (
             <div key={m.id} className='whitespace-pre-wrap'>
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
+              {m.role === 'user' ? (
+                <span className='text-slate-500'>{m.content}</span>
+              ) : (
+                <span className='text-green-900'>{m.content}</span>
+              )}
             </div>
           ))
         : null}
 
       <form onSubmit={handleSubmit}>
         <input
-          className='fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2'
+          className='bg-transparent border-none outline-none focus:ring-0 w-full max-w-md mb-8 text-slate-500'
           value={input}
-          placeholder='Say something...'
+          placeholder='Type something...'
           onChange={handleInputChange}
         />
       </form>
