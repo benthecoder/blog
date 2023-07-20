f5a1687263213abb054d305768c5a5b5062fb537
benthecoder <benthecoder07@gmail.com>
2023-07-19 21:35:12 -0700

    Change chat api and design
    Using OpenAIStream

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,29 +1,25 @@
-import { StreamingTextResponse, LangChainStream, Message } from 'ai';
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { AIMessage, HumanMessage } from 'langchain/schema';
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
 
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-
-  const { stream, handlers } = LangChainStream();
-
-  const llm = new ChatOpenAI({
-    streaming: true,
+  // Create a chat completion using OpenAIApi
+  const response = await openai.createChatCompletion({
+    model: 'gpt-4',
+    stream: true,
+    messages,
   });
 
-  llm
-    .call(
-      (messages as Message[]).map((m) =>
-        m.role == 'user'
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content)
-      ),
-      {},
-      [handlers]
-    )
-    .catch(console.error);
+  // Transform the response into a readable stream
+  const stream = OpenAIStream(response);
 
+  // Return a StreamingTextResponse, which can be consumed by the client
   return new StreamingTextResponse(stream);
 }
