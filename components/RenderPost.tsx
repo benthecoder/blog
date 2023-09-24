4ef12a8d562e94e93dd64b2dd3e8bd3bc0cdfcac
benthecoder <benthecoder07@gmail.com>
2023-09-23 21:49:19 -0500

    Change post render design

--- components/RenderPost.tsx
+++ components/RenderPost.tsx
@@ -4,18 +4,26 @@ import Link from 'next/link';
 const RenderPost = ({ ...post }) => {
   return (
     <div key={post.data.title}>
-      <p className='font-bold text-left mb-3 text-2xl'>{post.data.title}</p>
+      <h2 className='font-bold text-left mb-4 text-2xl'>{post.data.title}</h2>
       <article className='prose'>
         <Markdown children={post.content} />
       </article>
-      <div className='flex flex-row space-x-2 mt-10 text-green-900'>
-        {post.data.tags.split(', ').map((tag: any) => (
-          <Link href={`/tags/${tag}`} key={tag}>
-            #{tag}
-          </Link>
-        ))}
+      <div className='flex justify-between items-center pt-4 mt-5'>
+        <div className='flex flex-wrap'>
+          {post.data.tags.split(', ').map((tag: any) => (
+            <Link
+              href={`/tags/${tag}`}
+              key={tag}
+              className='mr-2 px-1 py-0.5 border rounded-lg border-black'
+            >
+              #{tag}
+            </Link>
+          ))}
+        </div>
+        <p className='text-gray-400'>
+          {new Date(post.data.date).toLocaleDateString()}
+        </p>
       </div>
-      <p className='text-slate-400 mt-2'>{post.data.date}</p>
     </div>
   );
 };
