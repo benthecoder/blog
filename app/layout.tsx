0ec59b49ac46e0c4c882fe39dde922073b4ac7ca
benthecoder <benthecoder07@gmail.com>
2023-10-03 00:54:32 -0500

    Change: sidebar component, hover over and text color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link';
-import type { Metadata } from 'next';
-import './globals.css';
-import { Averia_Serif_Libre } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+import { Averia_Serif_Libre } from 'next/font/google';
+import Link from 'next/link';
+import { Sidebar } from '../components/SideBar';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'benneo',
@@ -17,41 +18,11 @@ const averia = Averia_Serif_Libre({
   variable: '--font-averia',
 });
 
-const links = [
-  { path: '/thoughts', text: '💭' },
-  { path: '/hn', text: 'hn' },
-  { path: '/chat', text: 'chat' },
-  { path: '/tags', text: 'tags' },
-  { path: '/now', text: 'now' },
-  { path: '/library', text: 'library' },
-  { path: '/posts', text: 'archive' },
-  { path: '/contact', text: 'say hi' },
-];
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const Sidebar = (
-    <aside className='flex flex-col items-center md:w-[150px]'>
-      <div className='md:sticky md:top-20'>
-        <div className='font-bold text-center'>
-          <Link href='/'>
-            <h2>BENEDICT NEO</h2>
-          </Link>
-        </div>
-        <div className='flex flex-row space-x-3 md:flex-col md:mt-10 md:items-end'>
-          {links.map(({ path, text }) => (
-            <Link key={path} href={path}>
-              {text}
-            </Link>
-          ))}
-        </div>
-      </div>
-    </aside>
-  );
-
   return (
     <html lang='en' className={`${averia.variable} font-serif`}>
       <head>
@@ -61,7 +32,7 @@ export default function RootLayout({
         />
       </head>
       <body className='font-serif max-w-3xl flex flex-col mb-40 mx-auto mt-4 md:flex-row md:mt-20'>
-        {Sidebar}
+        <Sidebar />
         <main className=' flex-auto text-md min-w-0 max-w-xl mt-10 md:mt-0 px-4 md:pl-20'>
           {children}
         </main>
---
+++ components/SideBar.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+const links = [
+  { path: '/thoughts', text: '💭' },
+  { path: '/hn', text: 'hn' },
+  { path: '/chat', text: 'chat' },
+  { path: '/tags', text: 'tags' },
+  { path: '/now', text: 'now' },
+  { path: '/library', text: 'library' },
+  { path: '/posts', text: 'archive' },
+  { path: '/contact', text: 'say hi' },
+];
+
+export function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <aside className='flex flex-col items-center md:w-[150px]'>
+      <div className='md:sticky md:top-20'>
+        <div className='font-bold text-center'>
+          <Link href='/'>
+            <h2>BENEDICT NEO</h2>
+          </Link>
+        </div>
+        <div className='flex flex-row space-x-3 md:flex-col md:mt-10 md:items-end'>
+          {links.map(({ path, text }) => (
+            <Link
+              key={path}
+              href={path}
+              className={
+                pathname === path ? 'text-gray-500' : 'hover:underline'
+              }
+            >
+              {text}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </aside>
+  );
+}
