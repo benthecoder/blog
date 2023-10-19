bab4d1917ad80184da79fba15eb0286f09104527
benthecoder <benthecoder07@gmail.com>
2023-10-19 17:29:42 -0500

    Add favicon files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Averia_Serif_Libre } from 'next/font/google';
-import Link from 'next/link';
 import { Sidebar } from '../components/SideBar';
 import './globals.css';
 
@@ -25,12 +24,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className={`${averia.variable} font-serif`}>
-      <head>
-        <link
-          rel='icon'
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🌊</text></svg>"
-        />
-      </head>
       <body className='font-serif max-w-3xl flex flex-col mb-40 mx-auto mt-4 md:flex-row md:mt-20'>
         <Sidebar />
         <main className=' flex-auto text-md min-w-0 max-w-xl mt-10 md:mt-0 px-4 md:pl-20'>
