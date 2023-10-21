3230616dcc0b8eae8fd4c2aa5effb3f630184224
benthecoder <benthecoder07@gmail.com>
2023-10-21 11:02:36 -0500

    Added next: prev: at botton of posts
    required updating postmetadata interface, storing it on creation

--- components/getPostMetadata.ts
+++ components/getPostMetadata.ts
@@ -7,18 +7,20 @@ const getPostMetadata = (): PostMetadata[] => {
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith('.md'));
 
-  const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf8');
-    const matterResult = matter(fileContents);
+  const posts: PostMetadata[] = markdownPosts.map(
+    (fileName): Partial<PostMetadata> => {
+      const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf8');
+      const matterResult = matter(fileContents);
 
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      tags: matterResult.data.tags,
-      wordcount: (matterResult.content.match(/\b\w+\b/gu) || []).length,
-      slug: fileName.replace('.md', ''),
-    };
-  });
+      return {
+        title: matterResult.data.title,
+        date: matterResult.data.date,
+        tags: matterResult.data.tags,
+        wordcount: (matterResult.content.match(/\b\w+\b/gu) || []).length,
+        slug: fileName.replace('.md', ''),
+      };
+    }
+  ) as PostMetadata[];
 
   // Sort posts by date.
   posts.sort((a, b) => {
@@ -30,6 +32,11 @@ const getPostMetadata = (): PostMetadata[] => {
     } else {
       return -1;
     }
+  });
+
+  posts.forEach((post, index) => {
+    post.next = index > 0 ? posts[index - 1] : null;
+    post.prev = index < posts.length - 1 ? posts[index + 1] : null;
   });
 
   return posts;
