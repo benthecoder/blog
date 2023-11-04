18fb931f7961f5c642ada06800fb3c529be5289d
benthecoder <benthecoder07@gmail.com>
2023-11-04 12:32:19 -0500

    Add local font for og route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -7,15 +7,10 @@ export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const postTitle = searchParams.get('title');
 
-  // You would fetch the actual font file here. This URL is just an example.
-  const fontFileUrl = 'https://example.com/fonts/AveriaSerifLibre-Regular.ttf'; // Replace with the actual URL to your font file
-  const fontRes = await fetch(fontFileUrl);
-  if (!fontRes.ok) {
-    throw new Error(
-      `Failed to load the font from ${fontFileUrl}: ${fontRes.status}`
-    );
-  }
-  const fontData = await fontRes.arrayBuffer();
+  const font = fetch(
+    new URL('../../public/fonts/AveriaSerifLibre-Bold.ttf', import.meta.url)
+  ).then((res) => res.arrayBuffer());
+  const fontData = await font;
 
   return new ImageResponse(
     (
@@ -56,7 +51,6 @@ export async function GET(req: NextRequest) {
           name: 'Averia Serif Libre',
           data: fontData, // fontData is now an ArrayBuffer
           style: 'normal',
-          weight: 400, // Assuming 400 is a valid value for the Weight type
         },
       ],
     }
