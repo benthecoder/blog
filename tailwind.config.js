266f0e986f745dae077c06e23dd64d7ffec3c284
benthecoder <benthecoder07@gmail.com>
2024-01-08 16:54:15 +0800

    Fix backticks render

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,25 @@ module.exports = {
       fontSize: {
         xs: '0.22rem',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '&nbsp;&nbsp;',
+            },
+            'code::after': {
+              content: '&nbsp;&nbsp;',
+            },
+            code: {
+              background: '#ffeff0',
+              wordWrap: 'break-word',
+              boxDecorationBreak: 'clone',
+              padding: '.1rem .3rem .2rem',
+              borderRadius: '.2rem',
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
