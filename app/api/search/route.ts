e6a77124074ab0ca28fb281cd12e85e7ed6748b7
benthecoder <benthecoder07@gmail.com>
2024-01-24 23:13:43 +0800

    Add more debug statements

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -8,17 +8,20 @@ export const runtime = 'edge';
 
 export async function POST(request: Request) {
   try {
+    console.log('Received search request');
     const sql = neon(process.env.NEON_DATABASE_URL!);
+    console.log('NEON Database URL:', process.env.NEON_DATABASE_URL);
 
     const body = await request.json();
+    console.log('Request body:', body);
 
     const schema = z.object({
       query: z.string().min(4, 'Query must be at least 4 character long.'),
     });
 
     const validated = schema.safeParse(body);
-
     if (!validated.success) {
+      console.error('Validation failed:', validated.error.message);
       return NextResponse.json(
         {
           error: `Invalid request: ${validated.error.message}`,
@@ -28,7 +31,9 @@ export async function POST(request: Request) {
     }
 
     const { query } = validated.data;
+    console.log('Search query:', query);
     const embedding = await generateEmbedding(query);
+    console.log('Generated embedding:', embedding);
 
     const sqlQuery = `
       SELECT title, slug, date, tags, wordcount, content
@@ -38,12 +43,13 @@ export async function POST(request: Request) {
     `;
 
     const result = await sql(sqlQuery);
+    console.log('SQL query result:', result);
     return NextResponse.json({ data: result });
   } catch (e: unknown) {
     if (!(e instanceof Error)) {
       throw e;
     }
-
+    console.error('Error in POST function:', e.message);
     return NextResponse.json({ error: e.message || '' }, { status: 500 });
   }
 }
