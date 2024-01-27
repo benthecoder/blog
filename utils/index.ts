f9e01e239771d622e56601008a2faab868ac4bf0
benthecoder <benthecoder07@gmail.com>
2024-01-28 00:03:42 +0800

    remove prefix from env

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export const generateEmbedding = async (prompt: string) => {
     const res = await fetch('https://api.openai.com/v1/embeddings', {
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       method: 'POST',
       body: JSON.stringify({
