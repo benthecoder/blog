95b2196eb0bbc4caa3308d9e0420308a2ee0da4f
benthecoder <benthecoder07@gmail.com>
2024-03-01 10:06:48 +0800

    fix dark mode archive page text color

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -4,8 +4,8 @@ import { PostMetadata } from './PostMetadata';
 const PostPreview = (props: PostMetadata) => {
   return (
     <Link href={`/posts/${props.slug}`}>
-      <div className='flex justify-between items-center hover:bg-blue-100  cursor-crosshair hover:underline'>
-        <p className='text-black'>{props.title}</p>
+      <div className='flex justify-between items-center hover:bg-blue-100  dark:hover:bg-slate-700 cursor-crosshair hover:underline'>
+        <p className='text-black dark:text-white'>{props.title}</p>
         <p className='text-slate-400'>{props.date}</p>
       </div>
     </Link>
