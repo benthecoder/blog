050faa51c4f8b242662e7746e9229499f416f8e9
benthecoder <benthecoder07@gmail.com>
2024-12-04 01:17:11 -0800

    update total posts text in dark mode

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -15,33 +15,33 @@ const ArchivePage = () => {
 
   return (
     <div>
-      <h1 className='font-bold text-left mb-6 text-2xl'> Archive</h1>
+      <h1 className="font-bold text-left mb-6 text-2xl"> Archive</h1>
 
-      <div className='grid grid-cols-1 text-sm'>
-        <div className='flex flex-row space-x-2 mb-3'>
+      <div className="grid grid-cols-1 text-sm">
+        <div className="flex flex-row space-x-2 mb-3">
           <div>
             Filter by{' '}
-            <Link href='/tags' className='underline'>
+            <Link href="/tags" className="underline">
               tags
             </Link>
           </div>
-          <div className='text-black-400'>•</div>
+          <div className="text-black-400">•</div>
           <div>
-            <Link href='/random' className='underline'>
+            <Link href="/random" className="underline">
               random
             </Link>{' '}
             🎲
           </div>
-          <div className='text-black-400'>•</div>
+          <div className="text-black-400">•</div>
           <div>
-            <Link href='/search' className='underline'>
+            <Link href="/search" className="underline">
               search
             </Link>{' '}
             🔍
           </div>
         </div>
 
-        <p className='text-green-900 text-sm mb-10'>
+        <p className="text-green-900 dark:text-green-100 text-sm mb-10">
           Total: {totalBlogs} ({new Intl.NumberFormat().format(totalWordCount)}{' '}
           words)
         </p>
