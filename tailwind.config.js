38d2d2a734d17c0229f0326995e2fdf82fcde189
benthecoder <benthecoder07@gmail.com>
2024-12-09 15:20:17 -0800

    add 3rd to 6th of dec

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
           accent: '#927456',
           border: '#e6c9a8',
           tag: '#faf4eb',
+          phthalo: {
+            50: '#f0f9f7',
+            100: '#123524', // Darker phthalo green
+            200: '#123524/5', // 5% opacity phthalo green
+          },
         },
         // Dark mode
         dark: {
@@ -19,6 +24,11 @@ module.exports = {
           accent: '#7aa2f7',
           border: '#7aa2f7',
           tag: '#1e2030',
+          phthalo: {
+            50: '#123524/10', // 10% opacity
+            100: '#1c4f36', // Lighter phthalo green for dark mode
+            200: '#123524/15', // 15% opacity
+          },
         },
       },
       fontFamily: {
