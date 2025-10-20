72d91d133ceeca2f7f867c00457a1e757f7e466f
Benedict Neo 梁耀恩 <benthecoder07@gmail.com>
2025-10-20 10:49:41 -0500

    fix post directory

--- utils/getPostContent.ts
+++ utils/getPostContent.ts
@@ -1,9 +1,9 @@
 import matter from 'gray-matter';
 import fs from 'fs';
+import path from 'path';
 
 const getPostContent = (slug: string) => {
-  const folder = 'posts/';
-  const file = `${folder}${slug}.md`;
+  const file = path.join(process.cwd(), 'posts', `${slug}.md`);
   const content = fs.readFileSync(file, 'utf8');
   const matterResult = matter(content);
   // Remove the orig property (Uint8Array) which can't be serialized
