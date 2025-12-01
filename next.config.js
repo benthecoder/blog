b094c88bc8480f55a0525ad2debae86b87dc0db5
benthecoder <benthecoder07@gmail.com>
2025-11-30 22:17:33 -0600

    cache and image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ["image/webp", "image/avif"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 31536000, // 1 year
+  },
   experimental: {
     //serverExternalPackages: ['sharp', 'onnxruntime-node'],
   },
