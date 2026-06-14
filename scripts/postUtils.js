6e3c5755b50bb9888ba38305c85b0b7f41b71c41
benthecoder <benthecoder07@gmail.com>
2026-06-14 15:55:06 -0400

    remove dead code

--- scripts/postUtils.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export const getAllPostSlugs = () => {
-  const postsDirectory = path.join(process.cwd(), "posts");
-  const filenames = fs.readdirSync(postsDirectory);
-  const slugs = filenames.map((filename) => path.parse(filename).name);
-  return slugs;
-};
